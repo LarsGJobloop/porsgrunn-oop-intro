268ec0d8fb530d05152bbbc4a4f7f82eb2815241
larsg <lars.gunnar@jobloop.no>
2024-11-26 12:53:22 +0100

    setup program loop

--- Program.cs
+++ Program.cs
@@ -1,19 +1,22 @@
-﻿Console.WriteLine("Please write something, then hit 'enter'");
-string? userInput = Console.ReadLine();
-if (userInput == null)
+﻿while (true)
 {
-  throw new Exception("Could not read user input");
-}
+  Console.WriteLine("Please write something, then hit 'enter'");
+  string? userInput = Console.ReadLine();
+  if (userInput == null)
+  {
+    throw new Exception("Could not read user input");
+  }
 
-switch (userInput)
-{
-  case "borrow":
-    Console.WriteLine("Boorowing a book");
-    break;
-  case "return":
-    Console.WriteLine("Returning a book");
-    break;
-  default:
-    Console.Write("Unrecognized command");
-    break;
+  switch (userInput)
+  {
+    case "borrow":
+      Console.WriteLine("Boorowing a book");
+      break;
+    case "return":
+      Console.WriteLine("Returning a book");
+      break;
+    default:
+      Console.Write("Unrecognized command");
+      break;
+  }
 }
